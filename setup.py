ecc2d11caf43b5469c2d85ab7296f7802f846126
Vlad Arbatov <vladzima@users.noreply.github.com>
2023-11-18 15:11:41 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,25 @@
-# this file contains some placeholders
-# that are changed in a local copy if a release is made
-
 import setuptools
 
-README = 'README.md'  # the path to your readme file
-README_MIME = 'text/markdown'  # it's mime type
+README = 'README.md'
+README_MIME = 'text/markdown'
 
 with open(README, "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="topenai",  # placeholder (name of repo)
-    version="1",  # placeholder (tag of release)
-    author="vladzima",  # placeholder (owner of repo)
-    description="",  # placeholder (description of repo)
-    url="https://github.com/vladzima/topenai",  # placeholder (url of repo)
+    name="topenai",
+    version="0.0.1",
+    author="vladzima",
+    description="",
+    url="https://github.com/vladzima/topenai",
     long_description=long_description,
     long_description_content_type=README_MIME,
     packages=setuptools.find_packages(),
-    author_email="",  # the email of the repo owner
+    author_email="v.o.arbatov@gmail.com",
     classifiers=[
         "Programming Language :: Python",
         "Operating System :: OS Independent"
     ],
-    install_requires=[  # add required pypi packages here
+    install_requires=[  
     ]
 )
